793ec7537b66fb47687db26adf5a610c02d71eac
Daniel Espino Timón <akathorn@gmail.com>
2023-05-18 11:13:06 +0200

    Add fade in animation for posts

--- src/app/blog/post/post.component.ts
+++ src/app/blog/post/post.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { Post } from 'src/app/models/blog';
 
@@ -5,7 +6,16 @@ import { Post } from 'src/app/models/blog';
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('{{ time }}ms', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class PostComponent {
   @Input() post!: Post;
+  @Input() fadeInTime = 500;
 }
