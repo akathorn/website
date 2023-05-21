11a8de89dc2dea7939ffecca3e73ad9435358122
Daniel Espino Timón <akathorn@gmail.com>
2023-05-21 08:34:44 +0200

    Put toolbar over all pages

--- src/app/blog/floating-toolbar/floating-toolbar.component.ts
+++ src/app/blog/floating-toolbar/floating-toolbar.component.ts
@@ -7,51 +7,50 @@ import {
 } from '@angular/animations';
 import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
+const openStyle = style({
+  transform: 'translateY(0%) translateX(-50%)',
+});
+
+const closedStyle = style({
+  transform: 'translateY(-200%) translateX(-50%)',
+});
+
 @Component({
   selector: 'app-floating-toolbar',
   templateUrl: './floating-toolbar.component.html',
   styleUrls: ['./floating-toolbar.component.scss'],
   animations: [
     trigger('openClose', [
-      // ...
-      state(
-        'open',
-        style({
-          transform: 'translateX(-50%)',
-        })
-      ),
-      state(
-        'closed',
-        style({
-          transform: 'translateY(-200%) translateX(-50%)',
-        })
-      ),
+      state('open', openStyle),
+      state('closed', closedStyle),
       transition('open => closed', [animate('0.2s')]),
-      transition('closed => open', [animate('0.2s')]),
+      transition('closed => open', [animate('0.2s 0.1s')]),
     ]),
   ],
 })
 export class FloatingToolbarComponent implements OnInit, OnDestroy {
-  hideToolbar = true;
+  showToolbar = false;
   lastScrollTop = 0;
 
   constructor() {}
 
   ngOnInit(): void {
     console.log('Init');
-    this.hideToolbar = false;
+    setTimeout(() => {
+      this.showToolbar = true;
+    }, 200);
   }
 
   ngOnDestroy(): void {
     console.log('destroy');
-    this.hideToolbar = true;
+    this.showToolbar = false;
   }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const currentScrollTop =
       window.pageYOffset || document.documentElement.scrollTop;
-    this.hideToolbar = currentScrollTop > this.lastScrollTop;
+    this.showToolbar = currentScrollTop <= this.lastScrollTop;
     this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
   }
 }
