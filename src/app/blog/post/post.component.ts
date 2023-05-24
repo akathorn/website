12fcf98e1f7c6c03cefe493a2569b4d94e8ad5a1
Daniel Espino Timón <akathorn@gmail.com>
2023-05-24 10:58:36 +0200

    Add expand/collapse animations

--- src/app/blog/post/post.component.ts
+++ src/app/blog/post/post.component.ts
@@ -1,31 +1,47 @@
 import { Component, Input } from '@angular/core';
 import { Post } from 'src/app/models/blog';
 import { fadeInWithDelay } from '../../animations';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
-  animations: [fadeInWithDelay],
+  animations: [
+    fadeInWithDelay,
+    trigger('expandCollapse', [
+      state('collapsed', style({ height: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('500ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class PostComponent {
   @Input() post!: Post;
   @Input() fadeInDelay = 0;
   @Input() collapsible = false;
-  @Input() collapseThreshold = 300;
   isExpanded = false;
+  content = '';
 
-  get firstFragment(): string {
-    // Get first characters of first fragment
-    let fragment = this.post.content.slice(0, this.collapseThreshold);
-    if (fragment.length === this.collapseThreshold) {
-      fragment += '...';
-    }
-    return fragment;
+  get firstParagraph(): string {
+    return this.post.content.split('\n')[0];
+  }
+
+  get remainingParagraphs(): string {
+    return this.post.content.split('\n').slice(1).join('\n');
   }
 
   get isShortPost(): boolean {
-    return this.post.content.length < this.collapseThreshold;
+    return this.post.content.split('\n').length === 1;
   }
 
   toggleExpand(): void {
