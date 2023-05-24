cf98a9503320013f51aa1e08f2baa4c4a92513d1
Daniel Espino Timón <akathorn@gmail.com>
2023-05-24 10:13:33 +0200

    Add expand/collapse for posts

--- src/app/blog/post/post.component.ts
+++ src/app/blog/post/post.component.ts
@@ -11,4 +11,24 @@ import { fadeInWithDelay } from '../../animations';
 export class PostComponent {
   @Input() post!: Post;
   @Input() fadeInDelay = 0;
+  @Input() collapsible = false;
+  @Input() collapseThreshold = 300;
+  isExpanded = false;
+
+  get firstFragment(): string {
+    // Get first characters of first fragment
+    let fragment = this.post.content.slice(0, this.collapseThreshold);
+    if (fragment.length === this.collapseThreshold) {
+      fragment += '...';
+    }
+    return fragment;
+  }
+
+  get isShortPost(): boolean {
+    return this.post.content.length < this.collapseThreshold;
+  }
+
+  toggleExpand(): void {
+    this.isExpanded = !this.isExpanded;
+  }
 }
